fbbe322fbd3ddd24e68919a058e905b5bd899002
max963 <mg_morais@id.uff.br>
2023-07-25 11:14:07 -0300

    feat: removing dashboard

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -9,9 +9,7 @@ const Menu = ({children}: any) => {
                 <Flex>
                     <Flex flexDirection={'column'} margin={"0px 15px 0px 0px"}  gap={3}>
                         <Stack direction='column' spacing={4} align='center'>
-                            <Link to={"/"}>
-                                <Button className="menu-button" colorScheme='blue' variant='solid'>Dashboard</Button>
-                            </Link>
+
                             <Link to={"/category"}>
                                 <Button className="menu-button" colorScheme='blue' variant='solid'>Categorias</Button>
                             </Link>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
 import NotFound from "../pages/NotFound";
 import Category from "../pages/Category";
 import CreateCategory from "../pages/Category/create";
@@ -7,7 +6,7 @@ import CreateCategory from "../pages/Category/create";
 export function PublicRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<Category />} />
       <Route path="/category" element={<Category />} />
       <Route path="/new-category" element={<CreateCategory />} />
       <Route path="*" element={<NotFound />} />
